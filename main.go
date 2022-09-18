b4951cfec02733d92eb6e41b0f7a2ba07452c2fd
sgolinski <simon.golinski@gmail.com>
2022-09-18 18:42:40 +0200

    test

--- main.go
+++ main.go
@@ -3,22 +3,73 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/core/types"
+	"log"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
-
-	client, _ := ethclient.Dial("http://localhost:8545")
-
-	blockNumber, _ := client.BlockNumber(context.Background())
-	number := big.NewInt(int64(blockNumber))
-
-	block, _ := client.BlockByNumber(context.Background(), number)
-
-	for _, txn := range block.Transactions() {
-
-		fmt.Println(txn.AccessList().StorageKeys())
-
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	b, _ := client.BlockNumber(context.Background())
+	blockNumber := big.NewInt(int64(b))
+	block, err := client.BlockByNumber(context.Background(), blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, tx := range block.Transactions() {
+		fmt.Println(tx.Hash().Hex())        // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+		fmt.Println(tx.Value().String())    // 10000000000000000
+		fmt.Println(tx.Gas())               // 105000
+		fmt.Println(tx.GasPrice().Uint64()) // 102000000000
+		fmt.Println(tx.Nonce())             // 110644
+		fmt.Println(tx.Data())              // []
+		fmt.Println(tx.To().Hex())          // 0x55fE59D8Ad77035154dDd0AD0388D09Dd4047A8e
+
+		chainID, err := client.NetworkID(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if msg, err := tx.AsMessage(types.NewEIP155Signer(chainID), nil); err == nil {
+			fmt.Println(msg.From().Hex()) // 0x0fD081e3Bb178dc45c0cb23202069ddA57064258
+		}
+
+		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(receipt.Status) // 1
+	}
+
+	//blockHash := common.HexToHash("0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9")
+	//count, err := client.TransactionCount(context.Background(), blockHash)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//for idx := uint(0); idx < count; idx++ {
+	//	tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+	//}
+	//
+	//txHash := common.HexToHash("0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2")
+	//tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+	//fmt.Println(isPending)       // false
 }
