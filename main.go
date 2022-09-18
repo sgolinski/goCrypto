d6c68148c205e2556f24869dddbda6620403ac84
sgolinski <simon.golinski@gmail.com>
2022-09-18 17:06:40 +0200

    test

--- main.go
+++ main.go
@@ -17,15 +17,25 @@ func main() {
 	//var transactions []types.Transaction
 
 	for _, txn := range block.Transactions() {
+		fmt.Print("Hash ")
 		fmt.Println(txn.Hash().String())
+		fmt.Print("Value ")
 		fmt.Println(txn.Value().String())
+		fmt.Print("Access list ")
 		fmt.Println(txn.AccessList().StorageKeys())
+		fmt.Print("Nonce ")
 		fmt.Println(txn.Nonce())
+		fmt.Print("To ")
 		fmt.Println(txn.To().String())
+		fmt.Print("Gas price ")
 		fmt.Println(txn.GasPrice().String())
+		fmt.Print("Type ")
 		fmt.Println(txn.Type())
+		fmt.Print("Chain ID ")
 		fmt.Println(txn.ChainId().String())
+		fmt.Print("Cost ")
 		fmt.Println(txn.Cost().String())
+		fmt.Print("Data")
 		fmt.Println(txn.Data())
 
 		fmt.Println()
