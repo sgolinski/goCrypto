56bf7086bf8e35abb1d8a50ba6c9c83fc4e2acdb
sgolinski <simon.golinski@gmail.com>
2022-09-18 16:52:44 +0200

    test

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"math/big"
 )
 
 func main() {
 
 	client, _ := ethclient.Dial("http://localhost:8545")
 	blockNumber, _ := client.BlockNumber(context.Background())
-	fmt.Println(blockNumber)
+
+	block, _ := client.BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
+
+	fmt.Println(block)
 
 }
