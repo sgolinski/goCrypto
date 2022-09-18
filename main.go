6e4794ab58d9efe4ddcfb75438d646351797bcd6
sgolinski <simon.golinski@gmail.com>
2022-09-18 20:44:01 +0200

    wip

--- main.go
+++ main.go
@@ -30,18 +30,11 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println("Chain" + chainID.String())
 		msg, err := tx.AsMessage(types.NewEIP155Signer(chainID), nil)
 
 		fmt.Println("Message")
 		fmt.Println(msg.From().String())
 		fmt.Println(msg.To().String())
-		fmt.Println(msg.Gas())
-		fmt.Println(msg.GasPrice().String())
-		fmt.Println(msg.Value().String())
-		fmt.Println(msg.GasFeeCap().String())
-		fmt.Println(msg.IsFake())
-		fmt.Println(msg.AccessList().StorageKeys())
 
 		if err != nil {
 			log.Fatal(err)
@@ -52,41 +45,9 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println(receipt.Status) // 1
-		fmt.Println(receipt.Type)
-		fmt.Println(receipt.BlockNumber.String())
-		fmt.Println(receipt.TransactionIndex)
 		address := common.HexToAddress(receipt.ContractAddress.Hex())
 		fmt.Println(receipt.ContractAddress)
-		fmt.Println(address.Hex())
-		fmt.Println(receipt.Bloom)
-		fmt.Println(receipt.CumulativeGasUsed)
-		fmt.Println(receipt.GasUsed)
-		fmt.Println()
-		fmt.Println()
+		fmt.Println(address)
 	}
 
-	blockHash := common.HexToHash("0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9")
-	count, err := client.TransactionCount(context.Background(), blockHash)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for idx := uint(0); idx < count; idx++ {
-		tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-	}
-
-	txHash := common.HexToHash("0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2")
-	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-	fmt.Println(isPending)       // false
 }
