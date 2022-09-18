7c32792d09e81e0b2fe487d071488748f845f9d0
sgolinski <simon.golinski@gmail.com>
2022-09-18 19:06:19 +0200

    test

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func main() {
 			log.Fatal(err)
 		}
 
+		fmt.Println("Chain" + chainID.String())
 		msg, err := tx.AsMessage(types.NewEIP155Signer(chainID), nil)
 		v, r, s := tx.RawSignatureValues()
 		value := common.BigToAddress(v)
@@ -65,27 +66,27 @@ func main() {
 		fmt.Println(receipt.Status) // 1
 	}
 
-	//blockHash := common.HexToHash("0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9")
-	//count, err := client.TransactionCount(context.Background(), blockHash)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//for idx := uint(0); idx < count; idx++ {
-	//	tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
-	//	if err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	//	fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-	//}
-	//
-	//txHash := common.HexToHash("0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2")
-	//tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-	//fmt.Println(isPending)       // false
+	blockHash := common.HexToHash("0x9e8751ebb5069389b855bba72d94902cc385042661498a415979b7b6ee9ba4b9")
+	count, err := client.TransactionCount(context.Background(), blockHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for idx := uint(0); idx < count; idx++ {
+		tx, err := client.TransactionInBlock(context.Background(), blockHash, idx)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+	}
+
+	txHash := common.HexToHash("0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2")
+	tx, isPending, err := client.TransactionByHash(context.Background(), txHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(tx.Hash().Hex()) // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
+	fmt.Println(isPending)       // false
 }
