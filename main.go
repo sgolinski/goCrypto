2a1bc086b9673510780a731cce3c31950435901a
sgolinski <simon.golinski@gmail.com>
2022-09-18 16:45:34 +0200

    test

--- main.go
+++ main.go
@@ -1,35 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/gorilla/mux"
-	"log"
-	Handlers "main/handler"
-	"main/modules"
-	"net/http"
 )
 
 func main() {
-	// Create a client instance to connect to our providr
-	client, err := ethclient.Dial("http://localhost:8545")
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	latestBlock := modules.GetLatestBlock(*client)
-
-	fmt.Println(latestBlock)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Create a mux router
-	r := mux.NewRouter()
-
-	// We will define a single endpoint
-	r.Handle("/api/v1/eth/{module}", Handlers.ClientHandler{client})
-	log.Fatal(http.ListenAndServe(":8080", r))
+	client, _ := ethclient.Dial("http://localhost:8545")
+	blockNumber, _ := client.BlockNumber(context.Background())
+	fmt.Println(blockNumber)
 
 }
