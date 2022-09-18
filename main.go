79c9dc5b1e9662e8bc2423d2f96f2b7d3d5552f0
sgolinski <simon.golinski@gmail.com>
2022-09-18 18:46:39 +0200

    test

--- main.go
+++ main.go
@@ -24,21 +24,18 @@ func main() {
 	}
 
 	for _, tx := range block.Transactions() {
-		fmt.Println(tx.Hash().Hex())        // 0x5d49fcaa394c97ec8a9c3e7bd9e8388d420fb050a52083ca52ff24b3b65bc9c2
-		fmt.Println(tx.Value().String())    // 10000000000000000
-		fmt.Println(tx.Gas())               // 105000
-		fmt.Println(tx.GasPrice().Uint64()) // 102000000000
-		fmt.Println(tx.Nonce())             // 110644
-		fmt.Println(tx.Data())              // []
-		fmt.Println(tx.To().Hex())          // 0x55fE59D8Ad77035154dDd0AD0388D09Dd4047A8e
 
 		chainID, err := client.NetworkID(context.Background())
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if msg, err := tx.AsMessage(types.NewEIP155Signer(chainID), nil); err == nil {
-			fmt.Println(msg.From().Hex()) // 0x0fD081e3Bb178dc45c0cb23202069ddA57064258
+		msg, err := tx.AsMessage(types.NewEIP155Signer(chainID), nil)
+
+		fmt.Println(msg)
+
+		if err != nil {
+			log.Fatal(err)
 		}
 
 		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
