9a0b0cc07d407e90b1642bb3db9f4a8573ca16f2
sgolinski <simon.golinski@gmail.com>
2022-09-24 23:51:43 +0200

    test

--- handler/main.go
+++ handler/main.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"context"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
+	"myapp/consts"
 )
 
 // 0x10ED43C718714eb63d5aA57B78B54704E256024E panckake swap router v2
@@ -14,32 +16,88 @@ func ExtractBlock(client *ethclient.Client, block types.Block) {
 
 	for _, tx := range block.Transactions() {
 
-		err, msg := createMsg(client, tx)
+		if consts.ContainMethod(tx.Hash().String()) {
+			fmt.Println("TRANSACTION ")
+			fmt.Print("HASH ")
+			fmt.Println(tx.Hash().String())
+			fmt.Print("COST ")
+			fmt.Println(tx.Cost().String())
+			fmt.Print("NONCE ")
+			fmt.Println(tx.Nonce())
+			fmt.Print("TO ")
+			fmt.Println(tx.To().String())
+			fmt.Print("GAS ")
+			fmt.Println(tx.Gas())
+			fmt.Print("GAS PRICE ")
+			fmt.Println(tx.GasPrice().String())
+			fmt.Print("GAS FEE CAP ")
+			fmt.Println(tx.GasFeeCap().String())
+			fmt.Print("VALUE ")
+			fmt.Println(tx.Value().String())
+			fmt.Print("DATA ")
+			fmt.Println(common.BytesToHash(tx.Data()))
+			fmt.Print("TYPE ")
+			fmt.Println(tx.Type())
+			fmt.Println("END TRANSACTION ")
+			err, msg := createMsg(client, tx)
 
-		fmt.Print("From ")
-		fmt.Println(msg.From().String())
-		fmt.Print("To ")
-		fmt.Println(msg.To().String())
-		fmt.Println("Txn " + tx.Hash().String())
-		// cost is real value of transaction
-		fmt.Print("Cost ")
-		//cost := tx.Cost()
-		fmt.Println(" gwei")
-		if err != nil {
-			log.Fatal(err)
+			fmt.Println("Message: ")
+			fmt.Print("TO ")
+			fmt.Println(msg.To().String())
+			fmt.Print("FROM ")
+			fmt.Println(msg.From().String())
+			fmt.Print("GAS ")
+			fmt.Println(msg.Gas())
+			fmt.Print("VALUE ")
+			fmt.Println(msg.Value().String())
+			fmt.Print("IS FAKE ")
+			fmt.Println(msg.IsFake())
+			fmt.Print("GAS FEE CAP ")
+			fmt.Println(msg.GasFeeCap().String())
+			fmt.Print("GAS PRICE ")
+			fmt.Println(msg.GasPrice().String())
+			fmt.Print("NONCE ")
+			fmt.Println(msg.Nonce())
+			fmt.Println("END MESSAGE")
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, lg := range receipt.Logs {
+				fmt.Println()
+				fmt.Println("RECEIPT ")
+				fmt.Print("Address ")
+				fmt.Println(lg.Address)
+				address := lg.Address
+				fmt.Print("BLOCK NUMBER ")
+				fmt.Println(lg.BlockNumber)
+				fmt.Print("BLOCK HASH ")
+				fmt.Println(lg.BlockHash.String())
+				fmt.Print("TOPICS ")
+				fmt.Println(lg.Topics)
+				fmt.Print("REMOVED ")
+				fmt.Println(lg.Removed)
+				fmt.Print("INDEX ")
+				fmt.Println(lg.Index)
+				fmt.Print("TXHASH ")
+				fmt.Println(lg.TxHash.String())
+				fmt.Print("TXINDEX ")
+				fmt.Println(lg.TxIndex)
+				fmt.Print("Contract ")
+				fmt.Println(lg.Address)
+				contract, _ := client.BalanceAt(context.Background(), address, nil)
+				fmt.Print("CONTRACT: ")
+				fmt.Println(contract)
+			}
+			fmt.Println()
+		} else {
+			fmt.Println("Method not in interest")
 		}
-		receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, lg := range receipt.Logs {
-			fmt.Print("Address ")
-			fmt.Println(lg.Address)
-			address := lg.Address
-			contract, _ := client.BalanceAt(context.Background(), address, nil)
-			fmt.Println(contract)
-		}
-		fmt.Println()
+
 	}
 }
 
--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"myapp/handler"
-	"os"
 )
 
 // 0x10ED43C718714eb63d5aA57B78B54704E256024E panckake swap router v2
 // wei = 1000000000000000000
 func main() {
+	//	client, err := ethclient.Dial("wss://silent-serene-reel.bsc-testnet.discover.quiknode.pro/5924e8eb98190e32b10ec91104e3ca71ca126f92/")
 	client, err := ethclient.Dial("ws://127.0.0.1:8546")
 	if err != nil {
 		log.Fatal(err)
@@ -30,8 +30,7 @@ func main() {
 			log.Fatal(err)
 		case header := <-headers:
 			fmt.Print("Header: ")
-			fmt.Println(header)
-			os.Exit(1)
+			fmt.Println(header.Number.String())
 			block, err := client.BlockByHash(context.Background(), header.Hash())
 			if err != nil {
 				log.Fatal(err)
