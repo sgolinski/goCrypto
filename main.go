2119662735a189addcf5ccd8f908d638660206a7
sgolinski <simon.golinski@gmail.com>
2022-09-24 13:38:57 +0200

    mempool

--- main.go
+++ main.go
@@ -31,12 +31,13 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-
+			fmt.Println()
 			fmt.Println(block.Hash().Hex())        // 0xbc10defa8dda384c96a17640d84de5578804945d347072e091b4e5f390ddea7f
 			fmt.Println(block.Number().Uint64())   // 3477413
 			fmt.Println(block.Time())              // 1529525947
 			fmt.Println(block.Nonce())             // 130524141876765836
 			fmt.Println(len(block.Transactions())) // 7
+			fmt.Println()
 		}
 	}
 }
